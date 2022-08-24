33614f01f9b832a0144b14a135cd2d8aa39046e1
tmdgusya <tmdgusya@naver.com>
2022-08-24 23:20:54 +0900

    Create Function
    - return KtFile if file type is ktFile

--- src/main/kotlin/com/woowa/kotestboilerplate/utils/FileDescriptor.kt
+++ src/main/kotlin/com/woowa/kotestboilerplate/utils/FileDescriptor.kt
@@ -4,9 +4,11 @@ import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.psi.KtFile
 
 object FileDescriptor {
-    fun convertKotlinFile(psiFile: PsiFile) {
+    fun convertKotlinFile(psiFile: PsiFile): KtFile {
         if (psiFile::class != KtFile::class) {
             throw IllegalAccessException("You should use in kotlin file")
         }
+
+        return psiFile as KtFile
     }
 }
--- src/test/kotlin/com/woowa/kotestboilerplate/actions/FileDescriptorTest.kt
+++ src/test/kotlin/com/woowa/kotestboilerplate/actions/FileDescriptorTest.kt
@@ -2,20 +2,22 @@ package com.woowa.kotestboilerplate.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.woowa.kotestboilerplate.stub.PsiFileStub
+import com.intellij.psi.impl.source.PsiJavaFileImpl
 import com.woowa.kotestboilerplate.utils.FileDescriptor
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import org.jetbrains.kotlin.psi.KtFile
 
 
-class FileDescriptorTest: BehaviorSpec({
+class FileDescriptorTest : BehaviorSpec({
 
     given("In Java Class") {
+        val javaFiles = mockk<PsiJavaFileImpl>(relaxed = true)
         val actionMock = mockk<AnActionEvent>(relaxed = true) {
-            every { getData(CommonDataKeys.PSI_FILE) } returns PsiFileStub()
+            every { getData(CommonDataKeys.PSI_FILE) } returns javaFiles
         }
         `when`("When execute create boiler template method") {
             val exception = shouldThrow<IllegalAccessException> {
@@ -23,6 +25,19 @@ class FileDescriptorTest: BehaviorSpec({
             }
             then("cause exception that contain message that 'You should use in kotlin file' ") {
                 exception.message shouldBe "You should use in kotlin file"
+            }
+        }
+    }
+
+    given("In Kotlin Class") {
+        val ktFiles = mockk<KtFile>(relaxed = true)
+        val actionMock = mockk<AnActionEvent>(relaxed = true) {
+            every { getData(CommonDataKeys.PSI_FILE) } returns ktFiles
+        }
+        `when`("When execute create boiler template method") {
+            val ktFileClass = FileDescriptor.convertKotlinFile(actionMock.getData(CommonDataKeys.PSI_FILE)!!)
+            then("return Object that is type of ktFile ") {
+                ktFileClass::class shouldBe KtFile::class
             }
         }
     }
