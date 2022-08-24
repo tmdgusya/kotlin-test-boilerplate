8f428ab5609df319a7a8fabbff57fa51aabcf964
tmdgusya <tmdgusya@naver.com>
2022-08-24 23:23:50 +0900

    Add Description to convertKotlinFile

--- src/main/kotlin/com/woowa/kotestboilerplate/utils/FileDescriptor.kt
+++ src/main/kotlin/com/woowa/kotestboilerplate/utils/FileDescriptor.kt
@@ -4,6 +4,12 @@ import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.psi.KtFile
 
 object FileDescriptor {
+    /**
+     * @param psiFile is extract by psiManager when executed method
+     *
+     * @throws IllegalAccessException if file type is not kotlin file type
+     * @return KtFile (The file Type must be Kotlin File)
+     */
     fun convertKotlinFile(psiFile: PsiFile): KtFile {
         if (psiFile::class != KtFile::class) {
             throw IllegalAccessException("You should use in kotlin file")
