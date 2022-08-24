bfd6ed04b15b4e8d02d2e0745731f41565774900
tmdgusya <tmdgusya@naver.com>
2022-08-25 00:21:31 +0900

    Create AnalyzeKotlinClassHelper
    - Extract Method in Class From KtFile

--- src/main/kotlin/com/woowa/kotestboilerplate/helper/KotlinClassAnalyzeHelper.kt
+++ src/main/kotlin/com/woowa/kotestboilerplate/helper/KotlinClassAnalyzeHelper.kt
@@ -1,6 +1,7 @@
 package com.woowa.kotestboilerplate.helper
 
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
 import org.jetbrains.kotlin.psi.KtFile
 
 /**
@@ -12,6 +13,12 @@ class KotlinClassAnalyzeHelper(
 ) {
     fun getClass(className: String): PsiClass {
         return ktFile.classes.first { it.name == className }
+    }
+
+    fun getMethod(className: String, methodName: String): PsiMethod {
+        val clazz = getClass(className)
+
+        return clazz.allMethods.first { it.name == methodName }
     }
 
 }
--- src/test/kotlin/com/woowa/kotestboilerplate/helper/KotlinClassAnalyzeHelperTest.kt
+++ src/test/kotlin/com/woowa/kotestboilerplate/helper/KotlinClassAnalyzeHelperTest.kt
@@ -1,6 +1,7 @@
 package com.woowa.kotestboilerplate.helper
 
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -11,7 +12,7 @@ import org.jetbrains.kotlin.psi.KtFile
 
 class KotlinClassAnalyzeHelperTest : BehaviorSpec({
 
-    given("given ktFile") {
+    given("given ktFile and className") {
         val className = "TestClass"
         val mockPsiClass = mockk<PsiClass>(relaxed = true) {
             every { name } returns className
@@ -24,6 +25,27 @@ class KotlinClassAnalyzeHelperTest : BehaviorSpec({
             val clazz = kotlinClassAnalyzeHelper.getClass(className = className)
             then("return Class Name is given class Name")
             clazz.name shouldBe className
+        }
+    }
+
+    given("given ktFile and ClassName and specific methodName") {
+        val className = "TestClass"
+        val methodName = "testMethod"
+        val mockMethod = mockk<PsiMethod>(relaxed = true) {
+            every { name } returns methodName
+        }
+        val mockPsiClass = mockk<PsiClass>(relaxed = true) {
+            every { name } returns className
+            every { allMethods } returns arrayOf(mockMethod)
+        }
+        val ktFiles = mockk<KtFile>(relaxed = true) {
+            every { classes } returns arrayOf(mockPsiClass)
+        }
+        val kotlinClassAnalyzeHelper = KotlinClassAnalyzeHelper(ktFile = ktFiles)
+        `when`("when execute getClass(className)") {
+            val method = kotlinClassAnalyzeHelper.getMethod(className = className, methodName = methodName)
+            then("return Class Name is given class Name")
+            method.name shouldBe methodName
         }
     }
 })
